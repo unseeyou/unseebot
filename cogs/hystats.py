a18c2aa94ce8ca5a59e922425a826ec4dd0ea731
unseeyou <84846449+unseeyou@users.noreply.github.com>
2022-06-23 09:42:40 +1000

    Add files via upload

--- cogs/hystats.py
+++ cogs/hystats.py
@@ -31,7 +31,7 @@ def get_cache_data(playername):
     with open(f'cogs/{folder}/{filename}','r') as file:
         return json.load(file) # opens player's data file and gets the json inside
 
-def delete_old_data(ctx):
+def delete_old_data():
     try:
         files = os.listdir('cogs/hystats-data/')
         for file in files:
@@ -44,30 +44,64 @@ def delete_old_data(ctx):
                 pass
 
     except BaseException as err:
-        await ctx.send(f"Unexpected {err=}, {type(err)=}")
+        print(f"Unexpected {err=}, {type(err)=}")
 
-    print('deleted data')
     return True
 
 def create_embed(data):
     json = data # creates embed
     playerdata = json["player"]
     stats = playerdata["stats"]
+
     duels = stats["Duels"]
+    try:
+        duels_games = duels["games_played_duels"]
+    except BaseException:
+        duels_games = 0
+    try:
+        duels_wins = duels["wins"]
+    except BaseException:
+        duels_wins = 0
+
     bedwars = stats["Bedwars"]
+    try:
+        bedwars_games = bedwars["games_played_bedwars"]
+    except BaseException:
+        bedwars_games = 0
+    try:
+        bedwars_wins = bedwars["wins_bedwars"]
+    except BaseException:
+        bedwars_wins = 0
+
     skywars = stats["SkyWars"]
+    try:
+        skywars_games = skywars["games_played_skywars"]
+    except BaseException:
+        skywars_games = 0
+    try:
+        skywars_wins = skywars["wins"]
+    except BaseException:
+        skywars_wins = 0
+    try:
+        souls = skywars["souls"]
+    except BaseException:
+        souls = 0
+
     network_experience = int(playerdata["networkExp"])
     network_level = (math.sqrt((2 * network_experience) + 30625) / 50) - 2.5
     level = round(network_level, 2)
     uuid = playerdata["uuid"]
     player = json["player"]
-    if "rank" in player:
-        rank = player["rank"]
-    elif 'monthlyPackageRank' in player and player['monthlyPackageRank'] == "SUPERSTAR":
-        rank = 'MVP++'
-    elif "newPackageRank" in player and player["newPackageRank"] == 'VIP_PLUS' or 'MVP_PLUS':
-        rank = player["newPackageRank"].replace('_PLUS', '+')
-    else:
+    try:
+        if "rank" in player:
+            rank = player["rank"]
+        elif 'monthlyPackageRank' in player and player['monthlyPackageRank'] == "SUPERSTAR":
+            rank = 'MVP++'
+        elif "newPackageRank" in player and player["newPackageRank"] == 'VIP_PLUS' or 'MVP_PLUS':
+            rank = player["newPackageRank"].replace('_PLUS', '+')
+        else:
+            rank = 'Normal'
+    except BaseException:
         rank = 'Normal'
     username = player["displayname"]
     embed = discord.Embed(title='HyStats', colour=discord.Colour.dark_gold())
@@ -76,12 +110,12 @@ def create_embed(data):
     embed.add_field(name='Hypixel Rank', value=f"{username}'s Rank: {rank}")
     embed.add_field(name='Hypixel Level', value=f" Hypixel Level: {level}")
     embed.add_field(name='Duels Stats',
-                    value=f'Games Played: {duels["games_played_duels"]}, Games Won: {duels["wins"]}', inline=False)
+                    value=f'Games Played: {duels_games}, Games Won: {duels_wins}', inline=False)
     embed.add_field(name="Bedwars Stats",
-                    value=f'Games Played: {bedwars["games_played_bedwars"]}, Games Won: {bedwars["wins_bedwars"]}',
+                    value=f'Games Played: {bedwars_games}, Games Won: {bedwars_wins}',
                     inline=False)
     embed.add_field(name="Skywars Stats",
-                    value=f'Games Played: {skywars["games_played_skywars"]}, Games Won: {skywars["wins"]}, Souls: {skywars["souls"]}')
+                    value=f'Games Played: {skywars_games}, Games Won: {skywars_wins}, Souls: {souls}')
     embed.set_image(url=f'http://crafatar.com/renders/body/{uuid}.jpg?overlay')
     return embed
 
@@ -95,12 +129,15 @@ class stats(commands.Cog):
                 embed = create_embed(json)
                 await ctx.send(embed=embed)
 
-            except Exception:
-                await ctx.send('Error: Insufficient Data or Player does not exist/have a premium account')
+            except BaseException as error:
+                await ctx.send(f'Exception: {error}')
 
         elif not json["success"] and "recently" in json["cause"]:
-            embed = create_embed(get_cache_data(msg))
-            await ctx.send(embed=embed)
+            try:
+                embed = create_embed(get_cache_data(msg))
+                await ctx.send(embed=embed)
+            except BaseException as error:
+                await ctx.send(f'Exception from cache: {error}')
 
         elif json["success"] and json["player"] is None:
             await ctx.send('Error: Player not found')
@@ -108,7 +145,7 @@ class stats(commands.Cog):
         else:
             await ctx.send(f'Error: {json["cause"]}')
 
-        delete_old_data(ctx)
+        delete_old_data()
 
 def setup(bot):
     bot.add_cog(stats(bot))
